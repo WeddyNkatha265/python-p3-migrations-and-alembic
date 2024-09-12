dddc4e3e91d6addb056d369af1ccc4ab979456fe
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-09-13 00:08:59 +0300

    Updates migrations and alembic ish

---
+++ app/migrations/versions/4f4fc78cb067_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4f4fc78cb067
+Revises: 
+Create Date: 2024-09-12 23:56:32.344703
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4f4fc78cb067'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
